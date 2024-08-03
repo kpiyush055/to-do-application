d41ce66807b77b6709e5f0ddbc69e051b1d5b53a
kpiyush055 <kpiyush055@gmail.com>
2024-08-04 01:22:03 +0530

    initial frontend setup

---
+++ frontend/src/components/CrerateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo(){
+    return <>
+    <div>
+        <input type="text" placeholder="title"></input>
+        <input type="text" placeholder="description"></input>
+
+        <button>Add To Do</button>
+    </div>
+    </>
+}
