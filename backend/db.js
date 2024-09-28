7ffd4f94012867306650617f8934a9b161408af5
Piyush Kumar <102573996+kpiyush055@users.noreply.github.com>
2024-09-28 20:03:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://piyushkumardev1012:Skmkpkuk-8@cluster0.rom02zg.mongodb.net/todo")
+mongoose.connect("")
 
 const todoScheama = mongoose.Schema({
     title : String,
